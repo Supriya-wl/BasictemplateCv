3e352e6bf8979bcd07c525edc89408439236c957
Supriya Reddy <121212953+Supriya-wl@users.noreply.github.com>
2023-06-21 01:08:32 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import "./style.css";
+import About from './Components/About';
+import Section from './Components/Section';
+
+
 
 function App() {
+
+  let section_data=[
+    {
+      title:"Skills",
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+    },
+    {
+      title:"Experience",
+      description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+    },
+    {
+      title:"Education",
+      description:" It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+    }
+
+  ]
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <About></About>
+      {section_data.map((value,index)=>(
+      <Section title={value.title}
+      description={value.description}/
+      >))}
+      
+      
     </div>
   );
 }
---
+++ src/Components/Section.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Section= ({title,description})=> {
+
+  return (
+    <div className='section'>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <hr/>
+      
+    </div>
+  )
+}
+
+export default Section
